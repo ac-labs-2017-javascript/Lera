59f92bb6d079a6c90d6bf29280b669d555ee9c74
asd <asd>
2017-03-28 18:03:31 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log("Hello world");
+//console.log("Hello world");
+var fetch = require("node-fetch");
+var express = require("express");
+var cheerio = require("cheerio");
+
+var app = express();
+
+
+function processText(text){
+	var $ = cheerio.load(text);
+
+	var listItems = Array.from($("#tiles").children("li"));
+	return listItems.map(function(li){
+		return{
+			title : $(li).find(".title").text()
+		};
+	});
+}
+
+app.get("/hello", function(req, res){
+	var dopopocoSite = fetch("http://dopopoco.ro/meniu-individual-timisoara");
+	var textResponse = dopopocoSite.then(function(response){
+		return response.text();
+	});
+	textResponse.then(function(text){
+		res.send(processText(text));
+	});
+});
+
+app.listen(8099, function(){
+	console.log("Working 8099");
+});
