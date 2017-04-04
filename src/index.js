578f544637307bf6d66b5ab9cf43ee62584f23b6
asd <asd>
2017-04-04 17:52:13 +0300

    4.04 -v2

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@
 var fetch = require("node-fetch");
 var express = require("express");
 var cheerio = require("cheerio");
+var sqlite3 = require('sqlite3');
+
 
 var app = express();
-
+var db = new sqlite3.Database(':memory:');
 
 function processText(text){
 	var $ = cheerio.load(text);
@@ -31,10 +33,30 @@ app.get("/hello", function(req, res){
 		return response.text();
 	});
 	textResponse.then(function(text){
-		res.send(processText(text));
+		pizzas = processText(text);
+		pizzadb(pizzas);
+		res.send(pizzas);
+
 	});
 });
 
 app.listen(8099, function(){
 	console.log("Working 8099");
 });
+
+function pizzadb(pizzas){
+	db.serialize(function() {
+	  db.run("CREATE TABLE pizza (title TEXT, ingredients TEXT, pizzeria TEXT)");
+		 
+	  var stmt = db.prepare("INSERT INTO pizza(title, ingredients, pizzeria) VALUES (?, ?, ?)");
+	for (var i=0; i<pizzas.length; i++){
+		var p = pizzas[i];
+		stmt.run(p.title, p.ingredients, p.pizzeria);
+	  }
+
+  stmt.finalize();
+  db.each("SELECT rowid AS id, title, ingredients, pizzeria FROM pizza", function(err, row) {
+      console.log(row);
+  });
+});
+};
