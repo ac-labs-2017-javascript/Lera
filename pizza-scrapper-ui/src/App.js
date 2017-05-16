5f0f7ccafafe40efdc25f0fe9660c754f90d33e3
asd <asd>
2017-05-16 17:44:10 +0300

    16.05

--- pizza-scrapper-ui/src/App.js
+++ pizza-scrapper-ui/src/App.js
@@ -1,51 +1,104 @@
-import React from 'react';;
+import React from 'react';
 import './App.css';
 import ReactDOM from 'react-dom';
+import {Store, wrapStore} from "./redux.js"
 
 
-var state = {counter : 0};
 
 function reducer(state, action){
-  if (action.type === "INC"){
-    return {
-      counter : state.counter + 1
-    };
-  }else if (action.type === "DEC"){
-    return {
-      counter : state.counter - 1
-    };
-  }
   return state;
 }
 
-function handler(action){
-  state = reducer(state, action);
-  ReactDOM.render(
-    <App state={state}/>,
-    document.getElementById('root')
-  );
+
+function render(dispatch, getState){
+  var store = {
+    dispatch,
+    getState
+  };
+  return function(action){
+    ReactDOM.render(
+      <App store={store}/>,
+      document.getElementById('root')
+    );
+    return dispatch(action);
+  }
 }
 
-function Hello({name, counter}){
-  return(
-    <div>
-    <div>Hello {name} {counter}</div>
-    <button onClick={()=>handler({type:"INC"})}>Click (inc)</button>
-    <button onClick={()=>handler({type:"DEC"})}>Click (dec)</button>
-    
+var store = new Store({
+  pizzas : [{
+    url: "http://www.dopopoco.ro/images/produse/pizza_diavolo_small.png",
+    title : "Pizza Diavolo",
+    ingredients: "the fires of hell its self"
+  },
+  {
+    url: "http://www.dopopoco.ro/images/produse/pizza_diavolo_small.png",
+    title : "Pizza Diavolo",
+    ingredients: "the fires of hell its self"
+  },
+  {
+    url: "http://www.dopopoco.ro/images/produse/pizza_diavolo_small.png",
+    title : "Pizza Diavolo",
+    ingredients: "the fires of hell its self"
+  }]
+}, reducer);
+
+store = wrapStore(store, [render]);
+
+const cardStyle = {
+  maxWidth : "300px",
+  float : "left",
+  margin: 10
+
+};
+
+const cardImage = {
+  width : "100%"
+};
+
+const paddingMarginZero = {
+  padding : "0",
+  margin : "0",
+  marginBottom : "10"
+};
+
+
+
+function PizzaCard({url, title, ingredients}){
+  return (
+    <div className="w3-card-2" style={cardStyle}>
+      <img src={url} style={cardImage}/>
+        <div className="w3-container">
+        <h4>
+          <b>{title}</b>
+        </h4>
+        <p>
+          Ingrediente: {ingredients}
+        </p>
+      </div>
     </div>
   );
 }
 
+function App({store}){
+  const pizzas = store.getState().pizzas;
 
-function App({state}) {
+  const pizzaComponents = pizzas.map(function(pizza){
+    return (<PizzaCard title={pizza.title} url={pizza.url} ingredients={pizza.ingredients}/>);
+  });
+
+
   return (
     <div className="App">
-      <Hello name="You" counter={state.counter}/> 
+      <div className="w3-container" style={paddingMarginZero}>
+        <h1 className="w3-teal" style={paddingMarginZero}>Pizza Scrapper</h1>
+      </div>
+      <div className="w3-container">
+          {pizzaComponents}
+      </div>
     </div>
   );
 }
 
-handler({type : "INIT"})
+store.dispatch({ type : "_INIT_" });
 
 export default App;
